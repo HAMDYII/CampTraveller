66f534847fee7b7a3bf789c5a0f61c77db80c3d5
HAMDII <mohmedjamal228@gmail.com>
2024-02-17 19:15:34 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import Camp from "@/components/Camp";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
+  );
+}
